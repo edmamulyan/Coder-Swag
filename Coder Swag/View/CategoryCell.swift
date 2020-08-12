a481f0ba8a57c679864ab1a146e8b4e10b172cd8
Paul Franco <mail@paulfran.co>
2020-08-11 22:16:44 -0700

    set up navigation controller, added image assets, set up table cell

---
+++ Coder Swag/View/CategoryCell.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryCell.swift
+//  Coder Swag
+//
+//  Created by Paul Franco on 8/11/20.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
